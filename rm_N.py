4b9691a853633596d3e8499a15ba0f047027de17
hao dong <908407543@qq.com>
2018-04-15 11:24:17 +0800

    Add files via upload

--- rm_N.py
+++ rm_N.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python
-import sys
+import sys,numpy
 keep_line_nums_file=sys.argv[1]
 file=sys.argv[2]
 with open(keep_line_nums_file,'r')as f:
-	keep_line_nums=f.readlines()
-	keep2=keep_line_nums+1
+	keep=numpy.array(f.readlines(),dtype=numpy.int)
+	keep2=keep+1
 	keep3=keep2+1
 	keep4=keep3+1
 with open(file,'r')as f:
 	reads=f.readlines()
 	reads_new=[]
-	for n in range(len(keep_line_nums)):
-		reads_new.append(keep_line_nums[n])
-		reads_new.append(keep2)
-		reads_new.append(keep3)
-		reads_new.append(keep4)
+	for n in range(len(keep)):
+		reads_new.append(reads[keep[n]])
+		reads_new.append(reads[keep2[n]])
+		reads_new.append(reads[keep3[n]])
+		reads_new.append(reads[keep4[n]])
 with open(file[:-3]+'rm_N.fq','w')as f:
 	f.writelines(reads_new)
--- rm_N.sh
+++ rm_N.sh
@@ -4,5 +4,5 @@ read -p 'Input the list of files:' -t 30 namelist
 for n in `cat $namelist`
 do
 	awk 'NR%4==1&&$1!~/N/{print NR-1}' $n>${n}_line_num
-	python2 rm_N.py ${n}_line_num $n
+	python2 /data5/xqwang/dh/bin/extract_umi/rm_N.py ${n}_line_num $n
 done
