040a884fa5f6d09ecb2a0cd000abdbf614d390b0
hao dong <908407543@qq.com>
2018-04-15 08:43:55 +0800

    Add files via upload

--- rm_N.sh
+++ rm_N.sh
@@ -2,7 +2,7 @@
 #This script is created to remove reads which have N in umi.
 for n in `cat namelist`
 do
-	awk 'NR%4==1&&$1~/N/{print NR}' $n>j
+	awk 'NR%4==1&&$1!~/N/{print NR}' $n>j
 	for k in `cat j`
 	do
 		let m=$k+3
